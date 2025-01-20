376f6afbffc64150adc5cefb211ea0f8ce2842e4
zzangmmz <leemj036@gmail.com>
2025-01-20 19:22:58 +0900

    Initial Commit

---
+++ zzimtory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zzimtory
+//
+//  Created by 이명지 on 1/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
