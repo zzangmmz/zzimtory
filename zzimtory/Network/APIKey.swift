ea8f34c030c8dd30819a741a00d5f77d6e845814
zzangmmz <leemj036@gmail.com>
2025-01-24 16:23:02 +0900

    [feat[ Moya 사용한 API 리퀘스트 쿼리문 생성

--- zzimtory/Network/APIKey.swift
+++ zzimtory/Network/APIKey.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct APIKey {
-    let clientID: String = {
+    static let clientID: String = {
         Bundle.main.infoDictionary?["ClientID"] as! String
     }()
     
-    let clientSecret: String = {
+    static let clientSecret: String = {
         Bundle.main.infoDictionary?["ClientSecret"] as! String
     }()
     
-    let firebase: String = {
+    static let firebase: String = {
         let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
         let plistDict = NSDictionary(contentsOfFile: filePath!) as! [String: Any]
         
---
+++ zzimtory/Network/ShoppingAPI.swift
@@ -0,0 +1,47 @@
+//
+//  ShoppingAPI.swift
+//  zzimtory
+//
+//  Created by 이명지 on 1/24/25.
+//
+
+import Foundation
+import Moya
+
+enum ShoppingAPI {
+    case search(query: String)
+}
+
+extension ShoppingAPI: TargetType {
+    var baseURL: URL {
+        switch self {
+        case .search:
+            URL(string: "https://openapi.naver.com")!
+        }
+    }
+    
+    var path: String {
+        "/v1/search/shop.json"
+    }
+    
+    var method: Moya.Method {
+        .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .search(query: let query):
+            let parameters: [String: Any] = ["query": query.utf8]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        default:
+            ["X-Naver-Client-Id": APIKey.clientID,
+             "X-Naver-Client-Secret": APIKey.clientSecret]
+        }
+    }
+}
+
