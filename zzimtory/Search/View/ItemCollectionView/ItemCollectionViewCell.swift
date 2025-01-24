9d880a46b7adf42594d54f923f63868e9c4ad029
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-01-24 17:55:47 +0900

    [feat] 검색 결과를 보여줄 ItemCollectionViewCell 구성

---
+++ zzimtory/Search/View/ItemCollectionView/ItemCollectionViewCell.swift
@@ -0,0 +1,92 @@
+//
+//  ItemCollectionViewCell.swift
+//  zzimtory
+//
+//  Created by 김하민 on 1/24/25.
+//
+
+import UIKit
+import SnapKit
+
+final class ItemCollectionViewCell: UICollectionViewCell {
+    
+    static let id = "ItemCollectionViewCell"
+    
+    // MARK: - Cell components
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.layer.cornerRadius = 15
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+    }()
+    
+    private let priceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .black900Zt
+        
+        return label
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 10, weight: .medium)
+        label.textColor = .black900Zt
+        
+        return label
+    }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setCell(with item: Item) {
+        imageView.image = UIImage(systemName: item.image)
+        priceLabel.text = item.price
+        titleLabel.text = item.title
+    }
+    
+    // MARK: - UI functions
+    private func setUI() {
+        contentView.layer.cornerRadius = 16
+        contentView.backgroundColor = .white100Zt
+        
+        [imageView,
+         priceLabel,
+         titleLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+    }
+    
+    private func setConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview().inset(12)
+            make.height.equalTo(imageView.snp.width)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(imageView)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(imageView)
+            make.bottom.equalToSuperview().inset(12)
+        }
+    }
+}
