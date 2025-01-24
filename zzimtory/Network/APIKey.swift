0aaef8e5c3806b8eb81b939d33f3e1d801efcb62
zzangmmz <leemj036@gmail.com>
2025-01-24 18:06:53 +0900

    [refactor] 강제 언래핑 -> 옵셔널 바인딩으로 수정

--- zzimtory/Network/APIKey.swift
+++ zzimtory/Network/APIKey.swift
@@ -9,17 +9,28 @@ import Foundation
 
 struct APIKey {
     static let clientID: String = {
-        Bundle.main.infoDictionary?["ClientID"] as! String
+        guard let clientId = Bundle.main.infoDictionary?["ClientID"] as? String else {
+            fatalError("Client ID를 읽어오지 못했습니다.")
+        }
+        return clientId
     }()
     
     static let clientSecret: String = {
-        Bundle.main.infoDictionary?["ClientSecret"] as! String
+        guard let clientSecret = Bundle.main.infoDictionary?["ClientSecret"] as? String else {
+            fatalError("Client Secret을 읽어오지 못했습니다.")
+        }
+        return clientSecret
     }()
     
     static let firebase: String = {
-        let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
-        let plistDict = NSDictionary(contentsOfFile: filePath!) as! [String: Any]
+        guard let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+              let plistDict = NSDictionary(contentsOfFile: filePath) as? [String: Any] else {
+            fatalError("GoogleService-Info.plist를 찾지 못했습니다.")
+        }
         
-        return plistDict["API_KEY"] as! String
+        guard let apiKey = plistDict["API_KEY"] as? String else {
+            fatalError("Firebase API Key를 받아오지 못했습니다.")
+        }
+        return apiKey
     }()
 }
