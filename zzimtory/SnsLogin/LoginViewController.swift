7487d83e3d036cecec6645aeae02d28a05c99f71
zzangmmz <leemj036@gmail.com>
2025-01-27 18:08:45 +0900

    [feat] RxCocoa로 구글, 애플 로그인 버튼 탭 이벤트 추가

--- zzimtory/SnsLogin/LoginViewController.swift
+++ zzimtory/SnsLogin/LoginViewController.swift
@@ -10,10 +10,38 @@ import Firebase
 import FirebaseAuth
 import GoogleSignIn
 import RxSwift
+import RxCocoa
 
 final class LoginViewController: UIViewController {
+    private var loginView: LoginView?
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = LoginView(frame: view.frame)
+        loginView = LoginView(frame: view.frame)
+        bind()
+        view = loginView
+    }
+    
+    private func bind() {
+        loginView?.appleLoginButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.signInWithApple()
+            })
+            .disposed(by: disposeBag)
+        
+        loginView?.googleLoginButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.signInWithGoogle()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func signInWithApple() {
+        
+    }
+    
+    private func signInWithGoogle() {
+        
     }
 }
