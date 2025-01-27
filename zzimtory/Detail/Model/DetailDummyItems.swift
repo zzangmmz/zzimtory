dea28cfddfe0d62bd19df72266c6daa7e9c07d9a
soy <soysauce1223@naver.com>
2025-01-27 10:56:11 +0900

    [feat] 더미값 추가 (추후 삭제 예정)

---
+++ zzimtory/Detail/Model/DetailDummyItems.swift
@@ -0,0 +1,24 @@
+//
+//  DetailDummyItems.swift
+//  zzimtory
+//
+//  Created by seohuibaek on 1/27/25.
+//
+
+struct DetailDummyItems {
+    static let dummyItems: [Item] = [
+           Item(title: "스파오 콜라보 긴팔 파자마",
+                link: "https://example.com",
+                image: "dummy_image",
+                price: "29900",
+                mallName: "네이버",
+                productID: "123",
+                productType: "1",
+                brand: "스파오",
+                maker: "",
+                category1: "패션의류",
+                category2: "여성언더웨어/잠옷",
+                category3: "잠옷/홈웨어",
+                category4: "")
+       ]
+}
