3de65bd7fd7ac39d9cf9e3522571c9e9f755c951
zzangmmz <leemj036@gmail.com>
2025-01-27 14:32:31 +0900

    [feat] 로그인 뷰 프로퍼티 생성

--- zzimtory/SnsLogin/LoginView.swift
+++ zzimtory/SnsLogin/LoginView.swift
@@ -7,6 +7,19 @@
 
 import UIKit
 
-final class LoginView: UIView {
+final class LoginView: ZTView {
+    private let appleLoginButton = LoginButton(type: .apple)
+    private let googleLoginButton = LoginButton(type: .google)
+    private let kakaoLoginButton = LoginButton(type: .kakao)
+    private let naverLoginButton = LoginButton(type: .naver)
     
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [appleLoginButton,
+                                                       googleLoginButton,
+                                                       kakaoLoginButton,
+                                                       naverLoginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
 }
