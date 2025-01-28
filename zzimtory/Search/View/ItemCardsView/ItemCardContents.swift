d8b6187d10447c5f6702c90d10b7412e6a940240
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-01-28 20:58:44 +0900

    [chore] PR 후 머지 시 충돌을 일으킬 수 있는 코드 제거

--- zzimtory/Search/View/ItemCardsView/ItemCardContents.swift
+++ zzimtory/Search/View/ItemCardsView/ItemCardContents.swift
@@ -86,22 +86,3 @@ final class ItemCardContents: UIView {
         }
     }
 }
-
-// PR 올리기 전에 지울 것.
-extension UIImageView {
-    func loadImage(from url: URL) {
-        // URLSession을 통해 URL에서 비동기적으로 데이터를 가져오는 방법
-        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            if let error = error {
-                print("Error loading image: \(error.localizedDescription)")
-                return
-            }
-            
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
-            }
-        }.resume()
-    }
-}
