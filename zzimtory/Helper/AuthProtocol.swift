8b9fc462b6b29efbaaf292c44d0ff6abda12c89a
zzangmmz <leemj036@gmail.com>
2025-01-31 18:04:28 +0900

    [refactor] 구글, 애플 로그인 반환값 표준화

--- zzimtory/Helper/AuthProtocol.swift
+++ zzimtory/Helper/AuthProtocol.swift
@@ -5,9 +5,15 @@
 //  Created by 이명지 on 1/31/25.
 //
 
+import UIKit
+import GoogleSignIn
+
 /// 파이어베이스에서 제공하는 로그인 과정 추상화
 protocol NativeAuthProtocol: AnyObject {
-    func login()
+    func login(
+        from viewController: UIViewController,
+        comletion: @escaping (Result<UserCredential, Error>) -> Void
+    )
     func logout()
     func disconnect()
 }
@@ -18,4 +24,22 @@ protocol ThirdPartyAuthProtocol: AnyObject {
     func firbaseLogin()
     func logout()
     func disconnect()
+}
+
+/// 로그인 결과 표준화하는 구조체
+struct UserCredential {
+    let uid: String?
+    let email: String?
+    let nickname: String?
+    let token: String
+    
+    /// 구글 사용자 정보로부터 UserCredential 생성하는 생성자
+    init(from googleUser: GIDGoogleUser) {
+        self.uid = googleUser.userID
+        self.email = googleUser.profile?.email
+        self.nickname = googleUser.profile?.name
+        self.token = googleUser.accessToken.tokenString
+    }
+    
+    // 추후에 애플 유저 로그인도 추가
 }
