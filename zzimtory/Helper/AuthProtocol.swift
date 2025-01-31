0923e02669f474c3783caef13e0cfebfd0aa7426
zzangmmz <leemj036@gmail.com>
2025-01-31 19:03:00 +0900

    [refactor] 불필요한 UserCredential 삭제

--- zzimtory/Helper/AuthProtocol.swift
+++ zzimtory/Helper/AuthProtocol.swift
@@ -10,10 +10,7 @@ import GoogleSignIn
 
 /// 파이어베이스에서 제공하는 로그인 과정 추상화
 protocol NativeAuthProtocol: AnyObject {
-    func login(
-        from viewController: UIViewController,
-        comletion: @escaping (Result<UserCredential, Error>) -> Void
-    )
+    func login(from viewController: UIViewController)
     func logout()
     func disconnect()
 }
@@ -24,22 +21,4 @@ protocol ThirdPartyAuthProtocol: AnyObject {
     func firbaseLogin()
     func logout()
     func disconnect()
-}
-
-/// 로그인 결과 표준화하는 구조체
-struct UserCredential {
-    let uid: String?
-    let email: String?
-    let nickname: String?
-    let token: String
-    
-    /// 구글 사용자 정보로부터 UserCredential 생성하는 생성자
-    init(from googleUser: GIDGoogleUser) {
-        self.uid = googleUser.userID
-        self.email = googleUser.profile?.email
-        self.nickname = googleUser.profile?.name
-        self.token = googleUser.accessToken.tokenString
-    }
-    
-    // 추후에 애플 유저 로그인도 추가
 }
