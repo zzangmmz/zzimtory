f9db2c619cbd52033ff41d4bb7ad1b30288e5767
zzangmmz <leemj036@gmail.com>
2025-02-01 01:42:09 +0900

    [feat] 유저 등록 메서드 register() 추가

--- zzimtory/Helper/DatabaseManager.swift
+++ zzimtory/Helper/DatabaseManager.swift
@@ -7,6 +7,7 @@
 
 import FirebaseCore
 import FirebaseDatabase
+import FirebaseAuth
 
 final class DatabaseManager {
     private var ref: DatabaseReference!
@@ -14,5 +15,35 @@ final class DatabaseManager {
     init() {
         // 파이어베이스 참조 설정
         ref = Database.database().reference()
+    }
+    
+    func registerUser(email: String, nickname: String) {
+        guard let currentUser = Auth.auth().currentUser else {
+            print("현재 인증된 유저가 없습니다.")
+            return
+        }
+        
+        let uid = currentUser.uid
+        
+        let userData: [String: Any] = [
+            "email": email,
+            "nickname": nickname,
+            "uid": uid,
+            "pockets": []
+        ]
+        
+        ref.child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
+            if !snapshot.exists() {
+                self.ref.child("users").child(uid).setValue(userData) { error, _ in
+                    if let error = error {
+                        print("유저 등록 실패: \(error.localizedDescription)")
+                    } else {
+                        print("유저 등록 성공")
+                    }
+                }
+            } else {
+                print("이미 등록된 유저")
+            }
+        }
     }
 }
