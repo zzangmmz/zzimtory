fd06d35a63467b6efeacaf733074ba1d07bcb5c2
zzangmmz <leemj036@gmail.com>
2025-02-02 19:36:29 +0900

    [feat] 유저의 uid 읽어오는 getUserUID() 메서드 생성, 유저 주머니 데이터 업데이트하는 updatePockets() 메서드 생성

--- zzimtory/Helper/DatabaseManager.swift
+++ zzimtory/Helper/DatabaseManager.swift
@@ -11,20 +11,25 @@ import FirebaseAuth
 
 final class DatabaseManager {
     private var ref: DatabaseReference!
+    private var userUID: String?
     
     init() {
         // 파이어베이스 참조 설정
         ref = Database.database().reference()
+        getUserUID()
     }
     
-    /// 유저 등록 메서드
-    func registerUser(email: String, nickname: String) {
+    private func getUserUID() {
         guard let currentUser = Auth.auth().currentUser else {
             print("현재 인증된 유저가 없습니다.")
             return
         }
-        
-        let uid = currentUser.uid
+        self.userUID = currentUser.uid
+    }
+    
+    /// DB에 유저 등록하는 메서드
+    func createUser(email: String, nickname: String) {
+        guard let uid = self.userUID else { return }
         
         let userData: [String: Any] = [
             "email": email,
@@ -48,15 +53,9 @@ final class DatabaseManager {
         }
     }
     
-    /// 유저 데이터 읽어오는 메서드
-    func fetchUserData(completion: @escaping (User?) -> Void) {
-        guard let currentUser = Auth.auth().currentUser else {
-            print("현재 인증된 유저가 없습니다.")
-            completion(nil)
-            return
-        }
-        
-        let uid = currentUser.uid
+    /// DB에서 유저 읽어오는 메서드
+    func readUserData(completion: @escaping (User?) -> Void) {
+        guard let uid = self.userUID else { return }
         
         ref.child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let value = snapshot.value as? [String: Any] else {
@@ -73,6 +72,23 @@ final class DatabaseManager {
             } catch {
                 print("디코딩 실패")
                 completion(nil)
+            }
+        }
+    }
+    
+    /// DB에 유저 주머니 데이터 업데이트 하는 메서드
+    func updateUserPockets(newPockets: [Pocket], completion: @escaping (Bool) -> Void) {
+        guard let uid = self.userUID else { return }
+        
+        let updateData = ["pockets": newPockets]
+        
+        ref.child("users").child(uid).updateChildValues(updateData) { error, _ in
+            if let error = error {
+                print("주머니 업데이트 실패: \(error.localizedDescription)")
+                completion(false)
+            } else {
+                print("주머니 업데이트 성공")
+                completion(true)
             }
         }
     }
