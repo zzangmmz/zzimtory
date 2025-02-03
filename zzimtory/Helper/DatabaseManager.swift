a2713aa807816dede1edb9f7a430650b594c77da
zzangmmz <leemj036@gmail.com>
2025-02-03 13:45:36 +0900

    [feat] 싱글톤 패턴으로 수정

--- zzimtory/Helper/DatabaseManager.swift
+++ zzimtory/Helper/DatabaseManager.swift
@@ -10,10 +10,11 @@ import FirebaseDatabase
 import FirebaseAuth
 
 final class DatabaseManager {
+    static let shared = DatabaseManager()
     private var ref: DatabaseReference!
     private var userUID: String?
     
-    init() {
+    private init() {
         // 파이어베이스 참조 설정
         ref = Database.database().reference()
         getUserUID()
@@ -56,13 +57,12 @@ final class DatabaseManager {
     
     // MARK: - Data Read Method
     /// DB에서 유저 읽어오는 메서드
-    func readUserData(completion: @escaping (User?) -> Void) {
+    func readUserData() {
         guard let uid = self.userUID else { return }
         
         ref.child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let value = snapshot.value as? [String: Any] else {
                 print("유저를 찾지 못했습니다.")
-                completion(nil)
                 return
             }
             
@@ -70,17 +70,15 @@ final class DatabaseManager {
                 let jsonData = try JSONSerialization.data(withJSONObject: value)
                 
                 let user = try JSONDecoder().decode(User.self, from: jsonData)
-                completion(user)
             } catch {
                 print("디코딩 실패")
-                completion(nil)
             }
         }
     }
     
     // MARK: - Data Update Methods
     /// DB에 유저 주머니 데이터 업데이트 하는 메서드
-    func updateUserPockets(newPockets: [Pocket], completion: @escaping (Bool) -> Void) {
+    func updateUserPockets(newPockets: [Pocket]) {
         guard let uid = self.userUID else { return }
         
         let updateData = ["pockets": newPockets]
@@ -88,10 +86,8 @@ final class DatabaseManager {
         ref.child("users").child(uid).updateChildValues(updateData) { error, _ in
             if let error = error {
                 print("주머니 업데이트 실패: \(error.localizedDescription)")
-                completion(false)
             } else {
                 print("주머니 업데이트 성공")
-                completion(true)
             }
         }
     }
@@ -113,28 +109,25 @@ final class DatabaseManager {
     
     // MARK: - Data Delete Methods
     /// DB에서 유저 삭제하는 메서드
-    func deleteUser(completion: @escaping (Bool) -> Void) {
+    func deleteUser() {
         guard let uid = self.userUID else { return }
         
         ref.child("users").child(uid).removeValue() { error, _ in
             if let error = error {
                 print("유저 삭제 실패")
-                completion(false)
             } else {
                 print("유저 삭제 성공")
-                completion(true)
             }
         }
     }
     
     /// DB에서 유저의 주머니를 삭제하는 메서드
-    func deletePocket(title: String, completion: @escaping (Bool) -> Void) {
+    func deletePocket(title: String) {
         guard let uid = self.userUID else { return }
         
         ref.child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
             guard var userData = snapshot.value as? [String: Any],
                   var pockets = userData["pockets"] as? [Pocket] else {
-                completion(false)
                 return
             }
             
@@ -145,27 +138,23 @@ final class DatabaseManager {
                 self.ref.child("users").child(uid).updateChildValues(userData) { error, _ in
                     if let error = error {
                         print("유저 주머니 삭제 실패")
-                        completion(false)
                     } else {
                         print("유저 주머니 삭제 성공")
-                        completion(true)
                     }
                 }
             } else {
                 print("해당 타이틀의 주머니가 없습니다.")
-                completion(false)
             }
         }
     }
     
     /// 주머니 여러 개 삭제하는 메서드
-    func deletePockets(titles: [String], completion: @escaping (Bool) -> Void) {
+    func deletePockets(titles: [String]) {
         guard let uid = self.userUID else { return }
         
         ref.child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
             guard var userData = snapshot.value as? [String: Any],
                   var pockets = userData["pockets"] as? [Pocket] else {
-                completion(false)
                 return
             }
             
@@ -177,10 +166,8 @@ final class DatabaseManager {
             self.ref.child("users").child(uid).updateChildValues(userData) { error, _ in
                 if let error = error {
                     print("주머니들 삭제 실패")
-                    completion(false)
                 } else {
                     print("주머니 삭제 성공")
-                    completion(true)
                 }
             }
         }
