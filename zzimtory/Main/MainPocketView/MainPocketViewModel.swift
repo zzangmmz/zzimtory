c0dcb48860cb978bb98014caaaa433831ab006b4
zzangmmz <leemj036@gmail.com>
2025-02-05 00:40:01 +0900

    [refactor] onDataChanged() 메서드 삭제, addPocket() 메서드 파라미터 이름 name -> title로 수정

--- zzimtory/Main/MainPocketView/MainPocketViewModel.swift
+++ zzimtory/Main/MainPocketView/MainPocketViewModel.swift
@@ -16,41 +16,34 @@ class MainPocketViewModel {
     // 주머니 이름과 이미지 배열을 함께 관리
     private(set) var pockets: [Pocket] = []
     
-    var onDataChanged: (() -> Void)?
-    
-    func fetchData() {
+    func fetchData(completion: @escaping ([Pocket]) -> Void) {
         DatabaseManager.shared.readPocket { pockets in
             self.pockets = pockets
+            completion(pockets)
         }
     }
     
-    func addPocket(named name: String) {
+    func addPocket(title: String) {
         // 새 주머니 추가
-        let newPocket = Pocket(title: name, items: [Item](), image: "exampleImage")
-        pockets.append(newPocket)
+        let newPocket = Pocket(title: title, items: [Item](), image: "exampleImage")
+        self.pockets.append(newPocket)
         DatabaseManager.shared.createPocket(title: newPocket.title)
-        onDataChanged?()
     }
     
-    func pocketCount() -> Int {
-        return pockets.count
-    }
     func sortPockets(by order: SortOrder) {
-        
         let fixedPocket = pockets.first { $0.title == "전체보기"}
         var otherPockets = pockets.filter { $0.title != "전체보기" }
         
-           switch order {
-           case .newest:
-               otherPockets.sort { $0.title > $1.title } // 최신순 정렬
-           case .oldest:
-               otherPockets.sort { $0.title < $1.title } // 오래된 순 정렬
-           }
+        switch order {
+        case .newest:
+            otherPockets.sort { $0.title > $1.title } // 최신순 정렬
+        case .oldest:
+            otherPockets.sort { $0.title < $1.title } // 오래된 순 정렬
+        }
         if let fixedPocket = fixedPocket {
             pockets = [fixedPocket] + otherPockets
         } else {
             pockets = otherPockets
         }
-           onDataChanged?()  // 정렬 후 UI 갱신
-       }
+    }
 }
