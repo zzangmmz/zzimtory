fee6ffa5fb57995b1a46313c2b873f02e6ecc957
zzangmmz <leemj036@gmail.com>
2025-02-06 02:05:38 +0900

    [feat] AppleAuthManager 재생성 후 로그인 메서드 추가, LoginViewController에서 해당 코드 삭제

--- zzimtory/SnsLogin/LoginViewController.swift
+++ zzimtory/SnsLogin/LoginViewController.swift
@@ -11,6 +11,8 @@ import RxCocoa
 import CryptoKit
 import AuthenticationServices
 
+fileprivate var currentNonce: String?
+
 final class LoginViewController: UIViewController {
     private var loginView: LoginView?
     private let disposeBag = DisposeBag()
@@ -47,37 +49,4 @@ final class LoginViewController: UIViewController {
             .disposed(by: disposeBag)
     }
     
-}
-
-extension LoginViewController {
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        var randomBytes = [UInt8](repeating: 0, count: length)
-        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
-        if errorCode != errSecSuccess {
-            fatalError(
-                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-            )
-        }
-        
-        let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        
-        let nonce = randomBytes.map { byte in
-            // Pick a random character from the set, wrapping around if needed.
-            charset[Int(byte) % charset.count]
-        }
-        
-        return String(nonce)
-    }
-    
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
-    }
 }
