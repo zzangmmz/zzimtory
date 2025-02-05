797d0029caed8a7f175ccdd6601808560b690b8e
zzangmmz <leemj036@gmail.com>
2025-02-06 01:29:29 +0900

    [feat] AppleAuthManager 생성, ID 토큰에 들어갈 난수 생성하는 randomNonceString() 메서드 추가

---
+++ zzimtory/Helper/AppleAuthManager.swift
@@ -0,0 +1,31 @@
+//
+//  AppleAuthManager.swift
+//  zzimtory
+//
+//  Created by 이명지 on 2/6/25.
+//
+
+import CryptoKit
+import AuthenticationServices
+
+final class AppleAuthManager {
+    func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      var randomBytes = [UInt8](repeating: 0, count: length)
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+      if errorCode != errSecSuccess {
+        fatalError(
+          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+        )
+      }
+
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+      let nonce = randomBytes.map { byte in
+        charset[Int(byte) % charset.count]
+      }
+
+      return String(nonce)
+    }
+}
