79ac5a4ee731fd2bbcd51bbeea10bf2430cc70a2
zzangmmz <leemj036@gmail.com>
2025-02-06 01:39:02 +0900

    [feat] sha256() 메서드 LoginViewController로 이동

--- zzimtory/SnsLogin/LoginViewController.swift
+++ zzimtory/SnsLogin/LoginViewController.swift
@@ -70,4 +70,14 @@ extension LoginViewController {
         
         return String(nonce)
     }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
 }
