542036cc56f89a18fef81302be0e68ded1cc66ed
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-02-05 19:42:05 +0900

    [feat] 기본적인 마이페이지 UI 구현 완료

--- zzimtory/MyPage/MyPageViewController.swift
+++ zzimtory/MyPage/MyPageViewController.swift
@@ -10,9 +10,11 @@ import SnapKit
 
 final class MyPageViewController: UIViewController {
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    let tableViewContents: [(name: String, color: UIColor)] = [
+        ("이용약관", .black900Zt),
+        ("로그아웃", .black900Zt),
+        ("탈퇴하기", .systemRed)
+    ]
     
     // MARK: - UI Components
     private let logoImageView: UIImageView = {
@@ -24,11 +26,89 @@ final class MyPageViewController: UIViewController {
     }()
     
     private let userProfileView = UserProfileView()
-    private let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    private let tableView = UITableView(frame: .zero, style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        DatabaseManager.shared.readUserProfile { [weak self] response in
+            guard let nickname = response?.nickname,
+                  let email = response?.email else { return }
+            self?.userProfileView.setGreeting(for: nickname)
+            self?.userProfileView.setEmailAddress(to: email)
+        }
         
         view = ZTView(frame: view.frame)
+        
+        addComponents()
+        setTableView()
+        setConstraints()
+    }
+    
+    private func addComponents() {
+        [
+            logoImageView,
+            userProfileView,
+            tableView
+        ].forEach { view.addSubview($0) }
+    }
+    
+    private func setTableView() {
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isScrollEnabled = false
+
+        tableView.layer.cornerRadius = 15
+    }
+    
+    private func setConstraints() {
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalToSuperview().inset(24)
+            make.width.height.equalTo(40)
+        }
+        
+        userProfileView.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(12)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(74)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(userProfileView.snp.bottom).offset(24)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.bottom.equalTo(userProfileView.snp.bottom).offset(158)
+        }
+        
+    }
+    
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableViewContents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: String(describing: UITableViewCell.self))
+        
+        let content = tableViewContents[indexPath.item]
+        
+        cell.textLabel?.text = content.name
+        cell.textLabel?.textColor = content.color
+        cell.backgroundColor = .white100Zt
+        
+        
+        return cell
+    }
+    
+}
+
+extension MyPageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let content = tableViewContents[indexPath.item]
+        
+        print(content.name)
     }
 }
