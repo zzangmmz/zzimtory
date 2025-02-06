564275abcc0c013b1a25fe80a4ee06ab2fe00aee
soy <soysauce1223@naver.com>
2025-02-06 10:19:56 +0900

    [feat] 주머니 상세 화면과 아이템 상세화면 연결 구현

--- zzimtory/Main/PocketDetailView/PocketDetailViewModel.swift
+++ zzimtory/Main/PocketDetailView/PocketDetailViewModel.swift
@@ -18,4 +18,15 @@ class PocketDetailViewModel {
         self.pocket = pocket
         print(self.pocket)
     }
+        
+    func fetchData(completion: @escaping (Pocket) -> Void) {
+        DatabaseManager.shared.readPocket { [weak self] pockets in
+            if let updatedPocket = pockets.first(where: { $0.title == self?.pocket.title }) {
+                self?.pocket = updatedPocket
+                completion(updatedPocket)
+            } else {
+                completion(self?.pocket ?? Pocket(title: "", items: []))
+            }
+        }
+    }
 }
