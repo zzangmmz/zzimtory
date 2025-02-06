58c8d1fefb4ec97ba3f025a22d63fab7f3edc511
soy <soysauce1223@naver.com>
2025-02-06 14:49:07 +0900

    [fix] iPhoneSE에서 아이템 상세 화면 하단 부분이 너무 붙어있는 문제 해결

--- zzimtory/Detail/View/DetailView.swift
+++ zzimtory/Detail/View/DetailView.swift
@@ -231,7 +231,8 @@ final class DetailView: ZTView {
         }
         
         mainStackView.snp.makeConstraints { make in
-            make.edges.equalTo(scrollView)
+            make.top.horizontalEdges.equalTo(scrollView)
+            make.bottom.equalTo(scrollView).inset(16)
             make.width.equalTo(scrollView)
         }
     }
@@ -262,7 +263,7 @@ final class DetailView: ZTView {
     // [하단 스택] 웹사이트 버튼 및 주머니 넣기 버튼 스택, 구분선, 유사상품 레이블, 유사 상품 컬렌션 뷰
     private func setupBottomStackView() {
         bottomStackView.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalToSuperview().inset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
         }
         
         // 화면 내의 비율로 버튼 설정
