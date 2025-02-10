6b74419aeea15eced04e835611d4925fb157cad8
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-02-10 20:25:38 +0900

    [refactor] ItemSearchViewModel에 Input+Output/Transform 패턴 적용

--- zzimtory/Search/ViewModel/ItemSearchViewModel.swift
+++ zzimtory/Search/ViewModel/ItemSearchViewModel.swift
@@ -5,33 +5,32 @@
 //  Created by 김하민 on 1/26/25.
 //
 
+import Foundation
 import RxSwift
+import RxCocoa
 
 // ItemSearchView, ItemCardsView와 바인딩하여 사용하는 ItemSearchViewModel입니다.
 // SearchViewModel 프로토콜에 대한 설명은 SearchViewModel+Bindable 파일에 기재되어 있습니다.
-final class ItemSearchViewModel: SearchViewModel {
-    
-    let searchResult = BehaviorSubject(value: [Item]())
-    
-    private var query = ""
-    internal let disposeBag = DisposeBag()
+final class ItemSearchViewModel: ViewModel {
     private let shoppingRepository = ShoppingRepository()
-    
-    func search() {
-        shoppingRepository.fetchSearchData(query: query)
-            .subscribe(
-                onSuccess: { [weak self] (result: ShoppingAPIResponse) in
-                    self?.searchResult.onNext(result.items)
-                },
-                onFailure: { [weak self] error in
-                    self?.searchResult.onError(error)
-                }
-            )
-            .disposed(by: disposeBag)
+    var disposeBag = DisposeBag()
+
+    struct Input {
+        var query: Observable<String>
     }
     
-    func setQuery(to string: String) {
-        query = string
+    struct Output {
+        let searchResult: Driver<[Item]>
     }
     
+    func transform(input: Input) -> Output {
+        let searchResult = input.query
+            .withUnretained(self)
+            .flatMap { vm, query -> Observable<[Item]> in
+                return vm.shoppingRepository.fetchForViewModel(with: query)
+            }
+            .asDriver(onErrorDriveWith: .empty())
+        
+        return Output(searchResult: searchResult)
+    }
 }
