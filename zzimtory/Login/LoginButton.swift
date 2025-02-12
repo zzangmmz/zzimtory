a8d5244c6e506812bd85557c22d98743a9473e7e
zzangmmz <leemj036@gmail.com>
2025-02-12 19:23:33 +0900

    [refactor] configuration 만드는 createConfiguration() 메서드, 이미지 리사이징 하는 resizeIcon() 메서드 생성

--- zzimtory/Login/LoginButton.swift
+++ zzimtory/Login/LoginButton.swift
@@ -40,42 +40,51 @@ enum SNS: String {
 final class LoginButton: UIButton {
     init(sns: SNS) {
         super.init(frame: .zero)
-        
+        self.configuration = createConfiguration(sns: sns)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createConfiguration(sns: SNS) -> UIButton.Configuration {
         var config = UIButton.Configuration.filled()
-        
-        var finalImage = sns.icon
-        if sns != .apple { // 애플은 SF Symbol 사용하므로 제외
-            let size = CGSize(width: 28, height: 28)
-            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-            sns.icon?.draw(in: CGRect(origin: .zero, size: size))
-        } else {
-            let width = 28.0
-            let image = UIImage(named: "appleIcon")
-            let aspectRatio = (image?.size.height ?? 1.0) / (image?.size.width ?? 1.0)
-            let height = width * aspectRatio
-            
-            UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0.0)
-            image?.draw(in: CGRect(origin: .zero, size: CGSize(width: width, height: height)))
-        }
-        
-        finalImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        config.attributedTitle = AttributedString(sns.title,
-                                                  attributes: AttributeContainer([
-                                                    .font: UIFont.systemFont(ofSize: 19, weight: .semibold)
-                                                  ]))
-        config.image = finalImage
+        config.attributedTitle = AttributedString(
+            sns.title,
+            attributes: AttributeContainer([
+                .font: UIFont.systemFont(
+                    ofSize: 19,
+                    weight: .semibold
+                )
+            ]))
+        config.image = resizeIcon(for: sns)
         config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 20)
         config.imagePlacement = .leading
         config.imagePadding = 10
         config.baseBackgroundColor = sns.backgroundColor
         config.baseForegroundColor = sns.foregroundColor
         config.cornerStyle = .medium
-        self.configuration = config
+        return config
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func resizeIcon(for sns: SNS) -> UIImage? {
+        guard let originalIcon = sns.icon else { return nil }
+        
+        let size = sns == .apple ?
+        CGSize(
+            width: 28.0,
+            height: 28.0 * (originalIcon.size.height / originalIcon.size.width)
+        ) :
+        CGSize(
+            width: 28,
+            height: 28
+        )
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        originalIcon.draw(in: CGRect(origin: .zero, size: size))
+        let resizedIcon = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedIcon
     }
 }
