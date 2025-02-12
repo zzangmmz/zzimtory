959c835a656e62d8423b9acf48940d54dc3a6dea
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-02-12 11:09:46 +0900

    [refactor] SwipeCardStackDelegate를 Rx로 래핑한 RxSwipeCardStackDelegateProxy 작성

---
+++ zzimtory/Search/View/ItemCardsView/Shuffle/Classes/RxSwipeCardStackDelegateProxy.swift
@@ -0,0 +1,63 @@
+//
+//  RxSwipeCardStackDelegateProxy.swift
+//  zzimtory
+//
+//  Created by 김하민 on 2/12/25.
+//
+
+import RxSwift
+import RxCocoa
+
+extension SwipeCardStack: HasDelegate {
+    public typealias Delegate = SwipeCardStackDelegate
+}
+
+class RxSwipeCardStackDelegateProxy
+: DelegateProxy<SwipeCardStack, SwipeCardStackDelegate>
+, DelegateProxyType
+, SwipeCardStackDelegate {
+    
+    weak private(set) var cardStack: SwipeCardStack?
+    
+    init(cardStack: SwipeCardStack) {
+        self.cardStack = cardStack
+        super.init(parentObject: cardStack, delegateProxy: RxSwipeCardStackDelegateProxy.self)
+    }
+    
+    static func registerKnownImplementations() {
+        self.register { RxSwipeCardStackDelegateProxy(cardStack: $0) }
+    }
+}
+
+extension Reactive where Base: SwipeCardStack {
+    var delegate: DelegateProxy<SwipeCardStack, SwipeCardStackDelegate> {
+        return RxSwipeCardStackDelegateProxy.proxy(for: base)
+    }
+    
+    var didSelectCardAt: Observable<Int> {
+        return delegate
+            .methodInvoked(#selector(SwipeCardStackDelegate.cardStack(_:didSelectCardAt:)))
+            .map { num in
+                return num[1] as! Int
+            }
+    }
+    
+    var didSwipeAllCards: Observable<Void> {
+        return delegate
+            .methodInvoked(#selector(SwipeCardStackDelegate.didSwipeAllCards(_:)))
+            .map { _ in () }
+    }
+}
+
+//
+//@objc
+//optional func cardStack(_ cardStack: SwipeCardStack, didSelectCardAt index: Int)
+//
+//@objc
+//optional func cardStack(_ cardStack: SwipeCardStack, didSwipeCardAt index: Int, with direction: SwipeDirection)
+//
+//@objc
+//optional func cardStack(_ cardStack: SwipeCardStack, didUndoCardAt index: Int, from direction: SwipeDirection)
+//
+//@objc
+//optional func didSwipeAllCards(_ cardStack: SwipeCardStack)
