a58600358afe75882b6b692bb620876856abdb6c
zzangmmz <leemj036@gmail.com>
2025-02-12 13:32:58 +0900

    [feat] 주머니에 아이템 추가한 날짜가 기록되도록 saveDate 프로퍼티 추가

--- zzimtory/Model/UserModel.swift
+++ zzimtory/Model/UserModel.swift
@@ -5,6 +5,8 @@
 //  Created by 이명지 on 1/31/25.
 //
 
+import Foundation
+
 struct User: Codable {
     let email: String
     let nickname: String
@@ -14,6 +16,7 @@ struct User: Codable {
 
 struct Pocket: Codable {
     var title: String
+    var saveDate: Date
     var items: [Item]
     var images: [String] {
         if items.isEmpty {
@@ -24,11 +27,12 @@ struct Pocket: Codable {
     }
     
     private enum CodingKeys: String, CodingKey {
-        case title, items
+        case title, items, saveDate
     }
     
     init(title: String, items: [Item]) {
         self.title = title
+        self.saveDate = Date()
         self.items = items
     }
 }
