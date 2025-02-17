7decff11f96ec5697a0f9cd82d0782e882056dd5
zzangmmz <leemj036@gmail.com>
2025-02-17 15:06:46 +0900

    [refactor] 컬렉션뷰 세로 스크롤바 숨기기

--- zzimtory/Search/View/ItemSearchView/ItemSearchView.swift
+++ zzimtory/Search/View/ItemSearchView/ItemSearchView.swift
@@ -77,6 +77,7 @@ final class ItemSearchView: ZTView {
         itemCollectionView.register(ItemCollectionViewHeader.self,
                                     forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
                                     withReuseIdentifier: String(describing: ItemCollectionViewHeader.self))
+        itemCollectionView.showsVerticalScrollIndicator = false
         
         addSubview(itemCollectionView)
     }
