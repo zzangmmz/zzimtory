f0757fcf862c4d95efc9cd8ed13a12d75565402c
zzangmmz <leemj036@gmail.com>
2025-02-18 14:01:33 +0900

    [refactor] page 파라미터 추가

--- zzimtory/Network/ShoppingRepository.swift
+++ zzimtory/Network/ShoppingRepository.swift
@@ -11,20 +11,20 @@ import Moya
 import RxSwift
 
 protocol ShoppingRepositoryProtocol {
-    func fetchSearchData(query: String) -> Single<ShoppingAPIResponse>
+    func fetchSearchData(query: String, page: Int) -> Single<ShoppingAPIResponse>
 }
 
 final class ShoppingRepository: ShoppingRepositoryProtocol {
     private let provider = MoyaProvider<ShoppingAPI>()
     
     /// query 검색어에 대한 검색 결과 반환
-    func fetchSearchData(query: String) -> Single<ShoppingAPIResponse> {
-        provider.rx.request(.search(query: query))
+    func fetchSearchData(query: String, page: Int = 1) -> Single<ShoppingAPIResponse> {
+        provider.rx.request(.search(query: query, page: page))
             .map(ShoppingAPIResponse.self)
     }
     
-    func fetchForViewModel(with query: String) -> Observable<[Item]> {
-        provider.rx.request(.search(query: query))
+    func fetchForViewModel(with query: String, page: Int = 1, itemsPerPage: Int = 10) -> Observable<[Item]> {
+        provider.rx.request(.search(query: query, page: page))
             .map(ShoppingAPIResponse.self)
             .map { $0.items }
             .asObservable()
